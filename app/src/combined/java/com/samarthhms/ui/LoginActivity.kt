d57cdbb0a0d166ba56c19b5f5f0e541ea8b0beda
Sanket Patil <sanketpatil1309@gmail.com>
2023-01-23 17:08:14 +0530

    added error ui in login

--- app/src/combined/java/com/samarthhms/ui/LoginActivity.kt
+++ app/src/combined/java/com/samarthhms/ui/LoginActivity.kt
@@ -1,9 +1,18 @@
 package com.samarthhms.ui
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.DrawableContainer.DrawableContainerState
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.StateListDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import com.samarthhms.R
 import com.samarthhms.constants.Role
 import com.samarthhms.databinding.ActivityLoginBinding
 import com.samarthhms.domain.LoginResponseStatus
@@ -41,10 +50,11 @@ class LoginActivity : AppCompatActivity() {
             binding.loginButton.isClickable = false
         }
         loginViewModel.loginUserStatus.observe(this) {
+            Log.i("","Response is : $it")
             when(it){
                 LoginResponseStatus.SUCCESS -> onSuccess()
                 LoginResponseStatus.EXCEPTION -> onFailure()
-                LoginResponseStatus.WRONG_CREDENTIALS -> onFailure()
+                LoginResponseStatus.WRONG_CREDENTIALS -> onWrongCredentials()
                 else -> {}
             }
             binding.loginButton.isClickable = true
@@ -55,6 +65,21 @@ class LoginActivity : AppCompatActivity() {
     private fun onSuccess(){
         Toast.makeText(this, "SUCCESSFULLY LOGGED IN", Toast.LENGTH_SHORT).show()
 //        navigator.showDashboard(this@LoginActivity, roleSelected)
+    }
+
+    private fun onWrongCredentials(){
+        changeColorOfInputFields(binding.usernameTitle, binding.username, R.color.red)
+        changeColorOfInputFields(binding.passwordTitle, binding.password, R.color.red)
+    }
+
+    private fun changeColorOfInputFields(fieldTitle: TextView, fieldInput: EditText, color: Int){
+        val colorValue = getColor(color)
+        fieldTitle.setTextColor(colorValue)
+        val fieldInputDrawable = fieldInput.background as StateListDrawable
+        val dcs = fieldInputDrawable.constantState as DrawableContainerState
+        val drawableItem = dcs.children[0] as GradientDrawable
+        val pixels = R.dimen.login_edittext_background_stroke_width * resources.displayMetrics.density.toInt()
+        drawableItem.setStroke(pixels, colorValue)
     }
 
     private fun onFailure(){
--- app/src/combined/java/com/samarthhms/usecase/UseCase.kt
+++ app/src/combined/java/com/samarthhms/usecase/UseCase.kt
@@ -8,6 +8,7 @@ abstract class UseCase<out Type, in Params> where Type : Any {
 
     abstract suspend fun run(params: Params): Type
 
+    @OptIn(DelicateCoroutinesApi::class)
     operator fun invoke(
         params: Params,
         scope: CoroutineScope = GlobalScope,
